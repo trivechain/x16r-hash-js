f1fa47e3aeae3d2dccb589fad3826891235610c6
Corbin Fox <corbin.fox@gmail.com>
2018-12-27 16:55:52 -0700

    Conditionally exporting bindings (only if fs access is available).

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('bindings')('nodex16r.node')
+// Don't export bindings unless there is fs access.
+// Intended to disable x16r import in browser where extensions are unavailable.
+if (require('fs').accessSync) {
+    // console.log('Exporting x16r bindings.');
+    module.exports = require('bindings')('nodex16r.node');
+} else {
+    // console.log('Could not export x16r bindings (no fs access).');
+}
